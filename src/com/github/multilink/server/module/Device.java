fc674b237d56040d748ae712b91d2ccd6a118bc6
connection-lost <frankwu1995@hotmail.com>
2015-03-10 15:53:09 -0400

    Added basic variables

--- src/com/github/multilink/server/module/Device.java
+++ src/com/github/multilink/server/module/Device.java
@@ -1,5 +1,88 @@
 package com.github.multilink.server.module;
 
 public class Device {
+	
+	private int id;
+	private String name;
+	private String description = "<not set>";
+	
+	private double battery = -1;
+	private double hard_max = -1;
+	private double hard_now = -1;
+	private double ram_max = -1;
+	private double ram_now = -1;
+	
+	public Device(int _id, String _name){
+		id = _id;
+		name = _name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getBattery() {
+		return battery;
+	}
+
+	public void setBattery(double battery) {
+		this.battery = battery;
+	}
+
+	public double getHard_max() {
+		return hard_max;
+	}
+
+	public void setHard_max(double hard_max) {
+		this.hard_max = hard_max;
+	}
+
+	public double getHard_now() {
+		return hard_now;
+	}
+
+	public void setHard_now(double hard_now) {
+		this.hard_now = hard_now;
+	}
+
+	public double getRam_max() {
+		return ram_max;
+	}
+
+	public void setRam_max(double ram_max) {
+		this.ram_max = ram_max;
+	}
+
+	public double getRam_now() {
+		return ram_now;
+	}
+
+	public void setRam_now(double ram_now) {
+		this.ram_now = ram_now;
+	}
+	
+	
+	
+	
 
 }
