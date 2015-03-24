9d7664ab1f3b143b533744b8752df993f448c42a
connection-lost <frankwu1995@hotmail.com>
2015-03-24 18:19:28 -0400

    Use sessionmanager to group up sessions with utils

--- src/com/github/multilink/server/worker/SessionManager.java
+++ src/com/github/multilink/server/worker/SessionManager.java
@@ -1,5 +1,16 @@
 package com.github.multilink.server.worker;
 
+import com.github.multilink.server.storage.Session;
+
 public class SessionManager {
 
+	public boolean isActive(int deviceid){
+		if (Session.hasSession(deviceid)) return true;
+		else return false;
+	}
+	
+	
+	
+	
+	
 }
