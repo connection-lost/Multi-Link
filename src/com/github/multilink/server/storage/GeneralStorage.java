4e63dcabf6c623e033e20a6dfccead19de34bd5b
connection-lost <frankwu1995@hotmail.com>
2015-03-26 12:50:32 -0400

    Added Storage space for device and sessions

--- src/com/github/multilink/server/storage/GeneralStorage.java
+++ src/com/github/multilink/server/storage/GeneralStorage.java
@@ -1,5 +1,15 @@
 package com.github.multilink.server.storage;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.multilink.server.module.Device;
+
 public class GeneralStorage {
 
+	public List<Device> devices = new ArrayList<Device>();
+	public List<Session> sessions = new ArrayList<Session>();
+	
+	
+	
 }
