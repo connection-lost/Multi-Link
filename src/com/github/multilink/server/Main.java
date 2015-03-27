024534f882a045f12f07be36d962e4d9ebe298a8
connection-lost <frankwu1995@hotmail.com>
2015-03-27 16:28:10 -0400

    Add a small test

--- src/com/github/multilink/server/Main.java
+++ src/com/github/multilink/server/Main.java
@@ -1,11 +1,33 @@
 package com.github.multilink.server;
 
+import com.github.multilink.server.module.Device;
+import com.github.multilink.server.worker.SessionManager;
+
 public class Main {
 
 	public static void main(String args[]){
 		System.out.println("yoooooooooo");
 
 		System.out.println("Sida is Cute");
+		
+		test();
+		
+		
+		
+		
+		
+		
+	}
+	
+	public static void test(){
+		Device testdevice = new Device(0, "TestPhone");
+		testdevice.setBattery(100);
+		
+		System.out.println(SessionManager.createSession(testdevice).toString());
+		System.out.println(SessionManager.endSession(testdevice));
+		System.out.println(SessionManager.endSession(testdevice));
+		
+		
 	}
 	
 }
