19bbb9828fe1b4bbe5df18f84789cf6586f8d122
connection-lost <frankwu1995@hotmail.com>
2015-03-29 13:54:28 -0400

    Added function to create new device

--- src/com/github/multilink/server/storage/GeneralStorage.java
+++ src/com/github/multilink/server/storage/GeneralStorage.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.github.multilink.server.module.Device;
+import com.github.multilink.server.utils.EtcUtils;
 
 public class GeneralStorage {
 
@@ -11,5 +12,10 @@ public class GeneralStorage {
 	public static List<Session> sessions = new ArrayList<Session>();
 	
 	
+	
+	public static Device newDevice(String name){
+		Device device = new Device(EtcUtils.random.nextInt(Integer.MAX_VALUE), name);
+		return device;
+	}
 	
 }
