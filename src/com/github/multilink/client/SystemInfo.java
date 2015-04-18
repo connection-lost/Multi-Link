0e15eb9004c119ea840e81a56fb4b8af6d47831e
KeyboardNerd <chensidaa@gmail.com>
2015-04-18 12:29:47 -0400

    added JNA library to access kernel32

--- src/com/github/multilink/client/SystemInfo.java
+++ src/com/github/multilink/client/SystemInfo.java
@@ -1,7 +1,9 @@
 package com.github.multilink.client;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * 
@@ -11,8 +13,19 @@ import java.io.IOException;
  */
 public class SystemInfo {
 	private int deviceId;
+	
+	public static final String openiphoto = "pmset -g batt";
+	
 	public static void main(String[] args){
-		freeMemory();
+		
+		if (System.getProperty("os.name").equals("Mac OS X")){
+			System.out.println(exec(openiphoto)); // use command line to get battery status. 
+		}else{
+			// For windows
+			Kernel32.SYSTEM_POWER_STATUS batteryStatus = new Kernel32.SYSTEM_POWER_STATUS(); // use kernel to get battery status (windows)
+			Kernel32.INSTANCE.GetSystemPowerStatus(batteryStatus);
+			System.out.println(batteryStatus); // Shows result of toString() method.
+		}
 	}
 	// temporarily designed as this
 	public static void freeMemory(){
@@ -34,5 +47,46 @@ public class SystemInfo {
 	      System.out.println("Free space (GB): " + (float)root.getFreeSpace()/1024/1024/1024);
 	      System.out.println("Usable space (GB): " + (float)root.getUsableSpace()/1024/1024/1024);
 	    }
+	    exec(openiphoto);
+	    getSysInfo();
 	}
+	/**
+	 * System.getProperties don't provide battery life or more...
+	 */
+	public static void getSysInfo(){
+		System.out.println(System.getProperties().toString());
+	}
+	
+	/**
+	 * execute a command from java ( For Mac OSX, Linux and Windows) try this method to get system information. 
+	 * @param command
+	 * @return
+	 */
+
+	public static String exec(String command) {
+		StringBuffer output = new StringBuffer();
+ 
+		Process p;
+		try {
+			p = Runtime.getRuntime().exec(command);
+			p.waitFor();
+			BufferedReader reader = 
+                            new BufferedReader(new InputStreamReader(p.getInputStream()));
+ 
+                        String line = "";			
+			while ((line = reader.readLine())!= null) {
+				output.append(line + "\n");
+			}
+ 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+ 
+		return output.toString();
+	}
+	
+	/**
+	 * I find that the only way to communicate with system is by JNA or JNI Library. But any way it's non-general solution.
+	 * Support Mac OSX, Linux, Windows, requires different codes.
+	 */
 }
